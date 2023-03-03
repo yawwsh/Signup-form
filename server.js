f1f0aede72ea272bb77919b1c816b33a084ab2a1
Yash <yashshuklasocial@gmail.com>
2023-03-03 11:32:25 +0530

    added mailchimp api and getting data in json format in https request

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const https = require("https");
 const app = express();
 const path = require('path');
-var exphbs  = require('express-handlebars');
+// var exphbs  = require('express-handlebars');
 
 app.use(express.static(path.join(__dirname,"public")));
 
@@ -14,12 +14,46 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.post("/",(req,res)=>{
     var name = req.body.name;
     var email = req.body.email;
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: name,
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us10.api.mailchimp.com/3.0/lists/ce20253291"
+
+    const options = {
+        method: "POST",
+        auth: "sphere:562ff4de26ee46b4a418677e34b071ad-us10"
+        
+    }
+    const request = https.request(url,options,function(response){
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+
 });
 
-// app.get("/",(req,res)=>{
-//     res.sendFile(__dirname+"/public/signup.html");
-// });
+app.get("/",(req,res)=>{
+    res.render("signup");
+});
 
 app.listen(3000,function(){
     console.log("Server is up and running at port 3000");
 }); 
+
+// Mailchimp API key
+// 562ff4de26ee46b4a418677e34b071ad-us10
+
+// List ID
+// ce20253291
