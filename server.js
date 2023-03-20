dc559012dd638ceac23cc5aec0e8e44dc1cc8e7f
Yash <yashshuklasocial@gmail.com>
2023-03-20 20:09:44 +0530

    added the post request for try again method on failure page

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post("/",(req,res)=>{
                 email_address: email,
                 status: "subscribed",
                 merge_fields: {
-                    FNAME: name,
+                    FNAME: name
                 }
             }
         ]
@@ -35,6 +35,12 @@ app.post("/",(req,res)=>{
         
     }
     const request = https.request(url,options,function(response){
+        if(response.statusCode==200){
+            res.sendFile(__dirname+"/success.html");
+        }
+        else{
+            res.sendFile(__dirname+"/failure.html");
+        }
         response.on("data",function(data){
             console.log(JSON.parse(data));
         })
@@ -47,6 +53,11 @@ app.post("/",(req,res)=>{
 app.get("/",(req,res)=>{
     res.render("signup");
 });
+
+// post request for try again on failure page
+app.post("/failure",(req,res)=>{
+    res.redirect("/");
+})
 
 app.listen(3000,function(){
     console.log("Server is up and running at port 3000");
